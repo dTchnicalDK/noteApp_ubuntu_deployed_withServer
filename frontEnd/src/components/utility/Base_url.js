600f64e9b11ae569b7f9831248b1710a445b18f6
dTechnicalDK <dtechnicaldk@gmail.com>
2024-12-31 10:27:45 +0530

    Update Base_url.js

--- frontEnd/src/components/utility/Base_url.js
+++ frontEnd/src/components/utility/Base_url.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Base_url() {
   return (Base_url =
-    " https://noteapp-ubuntu-deployed-withserver.onrender.com");
+    "https://noteapp-ubuntu-deployed-withserver.onrender.com");
 }
 
 export default Base_url;
